6170d2fad6aafbd687e048e17c91d04a4bf24606
Eric Johnson <john4427@umn.edu>
2015-04-14 22:16:25 -0500

    pushing the add view controller add

--- ContactVieweriOS/MasterViewController.swift
+++ ContactVieweriOS/MasterViewController.swift
@@ -33,12 +33,13 @@ class MasterViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
-        }
+//        disabled the OOTB add code
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+//        self.navigationItem.rightBarButtonItem = addButton
+//        if let split = self.splitViewController {
+//            let controllers = split.viewControllers
+//            self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
+        //}
     
     }
 
