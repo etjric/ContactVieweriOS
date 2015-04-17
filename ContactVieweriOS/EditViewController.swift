273426584f7e0498960009daf50299135ed952e0
Eric Johnson <john4427@umn.edu>
2015-04-17 08:08:37 -0500

    added Add code to EditView

--- ContactVieweriOS/EditViewController.swift
+++ ContactVieweriOS/EditViewController.swift
@@ -19,23 +19,32 @@ class EditViewController: UIViewController {
     var detailItem: Contact?
     
     func configureView() {
-        // Update the user interface for the detail item.
-        if let detail: Contact = self.detailItem  {
-            if let nameField = self.nameField {
-                nameField.text = detail.name
+        
+        //we have a contact so we go into edit mode...
+        if (detailItem != nil) {
+            // Update the user interface for the detail item.
+            if let detail: Contact = self.detailItem  {
+                if let nameField = self.nameField {
+                    nameField.text = detail.name
+                }
+                if let titleField = self.titleField {
+                    titleField.text = detail.title
+                }
+                if let phoneField = self.phoneField {
+                    phoneField.text = detail.phone
+                }
+                if let emailField = self.emailField {
+                    emailField.text = detail.email
+                }
+                if let twitterField = self.twitterField {
+                    twitterField.text = detail.twitterId
+                }
             }
-            if let titleField = self.titleField {
-                titleField.text = detail.title
-            }
-            if let phoneField = self.phoneField {
-                phoneField.text = detail.phone
-            }
-            if let emailField = self.emailField {
-                emailField.text = detail.email
-            }
-            if let twitterField = self.twitterField {
-                twitterField.text = detail.twitterId
-            }
+        }
+        //we don't so we go into Add mode
+        else{
+            //nothing to set I think...
+        
         }
     }
     
@@ -50,15 +59,26 @@ class EditViewController: UIViewController {
     }
 
     @IBAction func saveContact(sender: AnyObject) {
-        if let detail: Contact = self.detailItem  {
-            detail.name = nameField.text
-            detail.phone = phoneField.text
-            detail.title = titleField.text
-            detail.email = emailField.text
-            detail.twitterId = twitterField.text
-            
-            ContactManager.sharedInstance.saveContacts()
-            navigationController?.popViewControllerAnimated(true)
+        //we have a detailItem, so we are in Edit mode
+        if (detailItem != nil) {
+            if let detail: Contact = self.detailItem  {
+                detail.name = nameField.text
+                detail.phone = phoneField.text
+                detail.title = titleField.text
+                detail.email = emailField.text
+                detail.twitterId = twitterField.text
+                
+            }
         }
+        
+        // we're adding the contact...
+        else {
+            let detail: Contact = Contact(name: nameField.text, phone: phoneField.text, title: titleField.text, email: emailField.text, twitterId: twitterField.text)
+            ContactManager.sharedInstance.addContact(detail)
+        }
+        //save and go back to prev...
+        ContactManager.sharedInstance.saveContacts()
+        navigationController?.popViewControllerAnimated(true)
+
     }
 }
